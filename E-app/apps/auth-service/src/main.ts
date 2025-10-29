db1861630c90b3bbaea47a83ebf14944aa242d05
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2025-10-29 17:00:10 +0200

    update

--- E-app/apps/auth-service/src/main.ts
+++ E-app/apps/auth-service/src/main.ts
@@ -15,9 +15,9 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
+    console.log(`Auth service is running at http://localhost:${port}/api`);
 });
 
 server.on("error", (err) => {
-    console.log("Server error: ", err);
+    console.log("Server Error: ", err);
 });
