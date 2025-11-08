908ab1ae8f9377dce71361f9aa66abc67f616eca
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2025-11-08 22:51:55 +0200

    update routers

--- E-app/apps/auth-service/src/routes/auth.router.ts
+++ E-app/apps/auth-service/src/routes/auth.router.ts
@@ -1,10 +1,13 @@
 import express, {Router} from "express";
-import { userLogin, userRegistration, verifyUser } from "../controller/auth.controller";
+import { forgotPassword, resetPassword, userLogin, userRegistration, verifyForgotPassword, verifyUser } from "../controller/auth.controller";
 
 const router:Router = express.Router();
 
 router.post("/user-registration", userRegistration);
 router.post("/verify-user", verifyUser);
 router.post("/user-login", userLogin);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.post("/verify-forgot-password", verifyForgotPassword);
 
 export default router;
