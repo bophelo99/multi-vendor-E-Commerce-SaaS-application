b9da2b151317c86723f962a4b09b9624768799b0
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2025-11-15 19:27:04 +0200

    uodate

--- E-app/apps/user-ui/src/app/(routes)/signup/page.tsx
+++ E-app/apps/user-ui/src/app/(routes)/signup/page.tsx
@@ -4,7 +4,7 @@ import GoogleButton from "apps/user-ui/src/shared/components/google-button";
 import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {useForm} from "react-hook-form";
 
 type FormData = {
@@ -17,7 +17,12 @@ const Signup = () => {
     // set useState hooks
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [serverError, setSeverError] = useState<string | null>(null);
-    const [canResend, setCanResend] = useState(false);
+    const [showOtp, setShowOtp] = useState(false);
+    const [canResend, setCanResend] = useState(true);
+    const [timer, setTimer] = useState(60);
+    const [otp, setOtp] = useState(["", "", "", ""]); // 4 digit code
+    const [userData, setUserData] = useState<FormData | null>(null);
+    const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
     // set router from next navigation
     const router = useRouter();
@@ -25,10 +30,25 @@ const Signup = () => {
     const {register, handleSubmit, formState: {errors}} = useForm<FormData>()
 
     //receive data from formdata
-    const onSubmit = (data:FormData) => {
+    const onSubmit = (data:FormData) => {};
 
-    };
+    //handleOtpChange is a function called whenever the user types into an OTP input box.
+    const handleOtpChange = (index:number, value:string) => {
+        //check otp is number
+        if(!/^[0-9]?$/.test(value)) return;
+        const newOtp = [... otp]
+        newOtp[index] = value;
+        setOtp(newOtp);
 
+        if(value && index  < inputRefs.current.length - 1){
+            inputRefs.current[index+1]?.focus();
+        }
+    }
+
+    //handle backspace to go back into previous otp backspace
+    const handleOtpkeyDown = (index:number, e:React.KeyboardEvent<HTMLInputElement>) => {
+
+    }
   return (
     <div className="w-full py-10 min-h-[85vh] bg-[f1f1f1]">
         <h1 className="text-4xl font-poppins font-semibold text-black text-center"> 
@@ -54,7 +74,9 @@ const Signup = () => {
                     <span className="px-3">or Sign in with Email</span>
                     <div className="flex-1 border-t border-gray-300"/>
                 </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
+                
+                {!showOtp ? (
+                 <form onSubmit={handleSubmit(onSubmit)}>
                     {/*Name*/}
                     <label className="block text-gray-700 mb-1">Name</label>
                     <input type="text" 
@@ -121,6 +143,23 @@ const Signup = () => {
                         <p className="text-red-500 text-sm mt-2">{serverError}</p>
                     )}
                 </form>
+                ) : (
+                    <div>
+                        <h3 className="text-xl font-semibold text-center mb-4">Enter verification code</h3>\
+                        <div className="justify-center flex gap-6">
+                            {otp ?.map((digit,index)=>(
+                                <input key={index} type="text" ref={(el)=> {
+                                  if (el) inputRefs.current[index] = el;
+                                }}
+                                maxLength={1}
+                                className="w-12 h-2 text-center border border-gray-300 outline-none !rounded"
+                                value={digit}
+                                onChange={(e) => handleOtpChange(index,e.target.value)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
       </div>
