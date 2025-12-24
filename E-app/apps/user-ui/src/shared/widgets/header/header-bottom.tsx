ef1335503f7eb257b48550a05db5feddec564b2f
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2025-12-24 08:23:18 +0200

    save

--- E-app/apps/user-ui/src/shared/widgets/header/header-bottom.tsx
+++ E-app/apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -34,7 +34,7 @@ const HeaderBottom = () => {
                         <AlignLeft color="white" />
                         <span className="text-white font-medium">All Departments</span>
                     </div>
-                    <ChevronDown color="blue" />
+                    <ChevronDown color="white" />
                 </div>
 
                 {/*Dropdown Menu*/}
